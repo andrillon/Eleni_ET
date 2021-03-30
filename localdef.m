8587983eda047091f447e0a1149b15320d1cd65f
Thomas Andrillon <thomas.andrillon@gmail.com>
2021-03-30 10:29:02 +0200

    adding the gitignore

--- localdef.m
+++
@@ -1,5 +0,0 @@
-
-% set-up paths and toolbox
-lscpTools_path='/Users/tand0009/WorkGit/LSCPtools/';
-edf2mat_path='/Users/tand0009/Work/local/edf-converter-master/';
-eyet_path='/Users/tand0009/Data/Eleni_ET/';
